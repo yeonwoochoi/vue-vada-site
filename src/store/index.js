07c29c15cbf027d29ec1f835b110af355a11f2f4
beagle <chldusdn20@gmail.com>
2021-12-20 13:18:38 +0900

    feat: binding research vuex

--- src/store/index.js
+++ src/store/index.js
@@ -9,6 +9,7 @@ import publications from './modules/publications/index'
 import professor from './modules/professor/index'
 import member from './modules/member/index'
 import utils from './modules/utils/index'
+import research from './modules/research/index'
 
 Vue.use(Vuex)
 
@@ -16,6 +17,7 @@ export default new Vuex.Store({
   modules: {
     app,
     user: user,
+    research: research,
     board: board,
     project: project,
     news: news,
